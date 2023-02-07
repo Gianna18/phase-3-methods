a2d058abc40bb3b291e4db5dd12544ce26adce22
Markmutuku <mark.mutuku@student.moringaschool.com>
2023-02-07 09:02:08 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+
+def greet_programmer 
+    puts "Hello, programmer!"
+
+end
+
+
+def greet (name)
+    puts "Hello, #{name}!"
+
+end
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+
+end 
+
+
+def add (num1, num2)
+    puts num1 +num2
+    return 2 + 5
+
+end
+
+def halve(integer)  
+   if integer.class == Integer
+    integer/2
+    else
+       nil
+end
+
+end
+ 
